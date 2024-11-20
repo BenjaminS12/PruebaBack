31fcc265c2b3d5033415b37d933a6e387a98e9d3
Ben11S <benjaminsotoferreira@gmail.com>
2024-11-20 03:07:26 -0300

    Mejora en el registro

--- eva03/appA/views.py
+++ eva03/appA/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'index.html')
+def signup(request):
+    
+    if request.method == 'GET':
+        return render(request, 'registro/signup.html', {
+            'form': UserCreationForm
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+            password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado exitosamente!')
+            except:
+                return render(request, 'registro/signup.html', {
+                     'form': UserCreationForm,
+                     "error":'Este Usuario ya existe'
+                 })
+        return render(request, 'registro/signup.html', {
+                     'form': UserCreationForm,
+                     "error":'Las contraseñas no coinciden'
+                 })
