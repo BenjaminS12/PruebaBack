cdbf2c534d2ec9d8f11797ea441f92ffd31db16d
Ben11S <benjaminsotoferreira@gmail.com>
2024-11-20 14:45:08 -0300

    Configuracion de las views

--- eva03/appA/views.py
+++ eva03/appA/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
-from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 
 def index(request):
@@ -17,19 +17,38 @@ def signup(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(username=request.POST['username'],
-            password=request.POST['password1'])
+                user = User.objects.create_user(
+                    username=request.POST['username'],password=request.POST
+                    ['password1'])
                 user.save()
-                return HttpResponse('Usuario creado exitosamente!')
-            except:
+                login(request, user)
+                return redirect('signin')
+            except IntegrityError:
                 return render(request, 'registro/signup.html', {
-                     'form': UserCreationForm,
-                     "error":'Este Usuario ya existe'
-                 })
+                    'form': UserCreationForm,
+                    "error":'Este usuario ya existe'
+                })
+        
         return render(request, 'registro/signup.html', {
-                     'form': UserCreationForm,
-                     "error":'Las contraseñas no coinciden'
-                 })
+            'form': UserCreationForm,
+            "error":'Las contraseñas no coinciden'
+        })
         
 def signin(request):
-    return render(request, 'registro/signin.html')
+    if request.method == 'GET':
+        return render(request, 'templates/registro/signin.html',{
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST
+            ['password'])
+        if user is None:
+            return render(request, 'templates/registro/signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Usuario o Contraseña incorrectos'
+            })
+        else:
+            login(request, user)
+            return redirect('index')
+        
